4431827468f927614e0e387509e4b05efac7bcd9
oceands <oceandsouza@yahoo.com>
2023-08-19 14:27:48 +0400

    COMMIT 4

--- PostgresDB.session.sql
+++ PostgresDB.session.sql
@@ -1,3 +1,12 @@
---this is for viewing all the tables 
+--SELECT ALL THE TABLES
+SELECT * FROM roles;
+SELECT * FROM tokenblocklist;
+SELECT * FROM user_roles;
+SELECT * FROM users;
+
+--DROP ALL TABLES
 Drop TABLE roles, tokenblocklist, user_roles,users;
-Drop TABLE roles, tokenblocklist, user_roles,users;
+
+--MANUALLY LINK USERS AND ROLES
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 1); -- Admin user
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 2); -- Regular user
