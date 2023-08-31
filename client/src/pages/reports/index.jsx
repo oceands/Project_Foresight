b59c9e2f56fb5cc74cf62860f67ca8df8d5d36c2
Uzair Naeem <uzairnaeem932@gmail.com>
2023-08-31 19:14:29 +0400

    report has been edited

--- client/src/pages/reports/index.jsx
+++ client/src/pages/reports/index.jsx
@@ -1,40 +1,56 @@
 import React from "react";
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box,Button, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import { mockDataInvoices } from "../../data/mockData";
-
 import Header from "../../components/Header";
+
 
 const Reports = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
+ 
   const columns = [
-    { field: "id", headerName: "Id" },
+    { field: "id", headerName: "Report ID" },
     {
       field: "name",
-      headerName: "Name",
+      headerName: "Title",
       width: 200,
       cellClassName: "name-column--cell",
+      flex: 0.5
     },
-    { field: "email", headerName: "Email", width: 200 },
-    { field: "phone", headerName: "Phone Number", width: 100 },
+    { field: "email", headerName: "Date Created", width: 200 },
+    { field: "phone", headerName: "Incident ID", width: 100 },
 
     {
       field: "cost",
-      headerName: "Cost",
+      headerName: "Created By",
       width: 100,
       renderCell: ({ row: { cost } }) => {
         return <Typography color={colors.pinkAccents[500]}>${cost}</Typography>;
       },
     },
-    { field: "date", headerName: "Date", width: 100 },
+    { field: "date", headerName: "Status", width: 100 },
+    {field: "action", headerName: "Action", width: 100,},
   ];
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="REPORTS" subtitle="welcome to your Reports" />
+      <Header title="REPORTS" subtitle="welcome to your Reports" />
+      <Button
+            sx={{
+              backgroundColor: colors.purpleAccent[700],
+        color: colors.grey[100],
+        fontSize: "14px",
+        fontWeight: "bold",
+        padding: "10px 20px",
+            }}
+          >
+             
+            Create Report
+          </Button>
+
       </Box>
       <Box
         m="8px 0 0 0"
@@ -42,16 +58,25 @@ const Reports = () => {
         sx={{
           "& .MuiDataGrid-root": {
             border: "none",
+            fontSize: "14px",
+            "& .MuiDataGrid-cell:focus": {
+              outline: "none", // Remove the focus outline
+            },
           },
+         
           "& .MuiDataGrid-cell": {
             borderBottom: "none",
           },
           "& .name-column--cell": {
-            color: colors.pinkAccents[300],
+            color: "#8cd2c6",
           },
           "& .MuiDataGrid-columnHeaders": {
             backgroundColor: colors.purpleAccent[700],
             borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaderTitle": {
+           
+            fontSize: "15px"
           },
           "& .MuiDataGrid-virtualScroller": {
             backgroundColor: colors.primary[400],
@@ -63,12 +88,14 @@ const Reports = () => {
           "& .MuiCheckbox-root": {
             color: `${colors.pinkAccents[200]} !important`,
           },
-          "& .MuiChackbox-root": {
-            color: `${colors.pinkAccents[200]} !important`,
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+            fontSize: "14px",
           },
         }}
       >
         <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+
       </Box>
     </Box>
   );
